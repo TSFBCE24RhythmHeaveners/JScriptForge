56b2a0cd489f0a9a37c0fb59e8fb9019b6b088d8
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-13 20:21:43 +0400

    Thank god! One oh fifth commit!

---
+++ examples/Wait Block.js
@@ -0,0 +1,24 @@
+Blockly.defineBlocksWithJsonArray([{
+  "type": "wait_seconds",
+  "message0": " wait %1 seconds",
+  "args0": [{
+    "type": "field_number",
+    "name": "SECONDS",
+    "min": 0,
+    "max": 86400,
+    "value": 1
+  }],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": "%{BKY_LOOPS_HUE}"
+}]);
+
+Blockly.JavaScript['wait_seconds'] = function(block) {
+  var seconds = Number(block.getFieldValue('SECONDS'));
+  var code = 'setTimeout(seconds * 1000);\n';
+  return code;
+}
+
+gvbvdxxScriptMaker.blocks.register([
+"wait_seconds"
+], "Wait Block", "green");

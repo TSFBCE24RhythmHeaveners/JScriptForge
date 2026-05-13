96488202ba2335da8cdc89cf2c9a0a8b2881d969
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-13 18:23:47 +0400

    Add the block into the editor.

---
+++ Editor/WaitBlock.js
@@ -0,0 +1,30 @@
+Blockly.defineBlocksWithJsonArray([
+{
+  "type": "pause",
+  "tooltip": "Pauses a script",
+  "helpUrl": "",
+  "message0": "pause %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "pauseseconds",
+      "align": "RIGHT",
+      "check": "Number"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 60,
+  "inputsInline": true
+}               
+]);
+Blockly.JavaScript['pause'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'setTimeout(${pauseseconds} * 1000);\n';
+  return code;
+};
+
+//Add blocks
+gvbvdxxScriptMaker.blocks.register([
+"pause"
+], "Wait Block", "gold");

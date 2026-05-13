442542f5dd84f251d03f4b1019b8b2d3fd8709df
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-13 19:54:41 +0400

    e

--- Editor/WaitBlock.js
+++ Editor/WaitBlock.js
@@ -1,18 +1,28 @@
-Blockly.defineBlocksWithJsonArray([{
-  "type": "rest",
-  "tooltip": "This is ma vare first bloc i made",
+const pausescript = {
+  "type": "pause",
+  "tooltip": "Pauses a script",
   "helpUrl": "",
-  "message0": "rest %1 seconds",
+  "message0": "pause %1",
   "args0": [
     {
       "type": "input_value",
-      "name": "restseconds",
+      "name": "pauseseconds",
       "align": "RIGHT",
       "check": "Number"
     }
   ],
   "previousStatement": null,
   "nextStatement": null,
-  "colour": %{BKY_LOOPS_HUE},
+  "colour": 60,
   "inputsInline": true
-}])
+};
+Blockly.common.defineBlocksWithJsonArray([pausescript]); {
+  const pauseseconds = block.getFieldValue('pauseseconds');
+  const code = `setTimeout(pauseseconds * 1000);\n`;
+  return code;
+};
+
+//Add blocks
+Blockly.blocks.register([
+"pause"
+], "Pause Block", "yellow");

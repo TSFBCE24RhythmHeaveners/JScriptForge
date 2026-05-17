b8074f597dd296329b72eb04086582480580a5e8
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-17 21:32:41 +0400

    e

--- Editor/pause_block.js
+++ Editor/pause_block.js
@@ -1,16 +1,18 @@
 Blockly.common.defineBlocksWithJsonArray([{
-  "type": "wait_seconds",
-  "message0": " wait %1 seconds",
+  "type": "pause",
+  "message0": "pause for %1 milliseconds",
   "args0": [{
     "type": "field_number",
-    "name": "SECONDS",
+    "name": "pausetime",
     "min": 0,
-    "max": 600,
+    "max": 86400000,
     "value": 1
   }],
   "previousStatement": null,
   "nextStatement": null,
   "colour": "%{BKY_LOOPS_HUE}"
+  "tooltip": "Pause for a specific amount of milliseconds",
+  "helpUrl": ""
 }]);
 Blockly.JavaScript['pause'] = function(block) {
   var code = 'setTimeout(' + pausetime + ');\n';

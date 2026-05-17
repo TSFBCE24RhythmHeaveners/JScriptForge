a25f1b30c415e2a7d04bcf811229514f8e16cf70
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-17 20:43:40 +0400

    The pause block.

--- Editor/pause_block.js
+++ Editor/pause_block.js
@@ -1,31 +1,19 @@
 Blockly.defineBlocksWithJsonArray([{
   "type": "pause",
-  "message0": "pause for %1 milliseconds, then do %2 %3",
-  "args0": [
-    {
-      "type": "input_value",
-      "name": "mseconds",
-      "check": "Number",
-      "align": "RIGHT"
-    },
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_statement",
-      "name": "afterpausing"
-    }
-  ],
+  "message0": "pause %1 milliseconds",
+  "args0": [{
+    "type": "field_number",
+    "name": "pausetime",
+    "min": 0,
+    "max": 86400000,
+    "value": 1
+  }],
   "previousStatement": null,
   "nextStatement": null,
-  "colour": %{BKY_LOOPS_HUE},
-  "tooltip": "Pause for a specific amount of milliseconds. (Maximum: 1 day)",
-  "helpUrl": "https://www.sitepoint.com/delay-sleep-pause-wait/"
+  "colour": "%{BKY_LOOPS_HUE}"
 }]);
 
 Blockly.JavaScript['pause'] = function(block) {
-  var pausing_mseconds = Blockly.JavaScript.valueToCode(block, 'mseconds', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_afterpausing = Blockly.JavaScript.statementToCode(block, 'afterpausing');
-  var code = 'setTimeout(() => {  ' + statements_afterpausing + ' },(' + pausing_mseconds + '));\n';
+  var code = 'setTimeout(' + pausetime + ');\n';
   return code;
-};
+}

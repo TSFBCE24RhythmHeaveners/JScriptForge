465648dc57d1cbd64616225a94e1af09969c7545
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-18 09:02:48 +0400

    It just works

--- Editor/pause_block.js
+++ Editor/pause_block.js
@@ -1,13 +1,16 @@
 Blockly.defineBlocksWithJsonArray([
 {
   "type": "pause",
-  "message0": "pause for %1 milliseconds then do %2",
+  "message0": "pause for %1 milliseconds then do %2 %3",
   "args0": [
     {
       "type": "input_value",
-      "name": "pausetime",
+      "name": "milliseconds",
       "check": "Number",
       "align": "RIGHT"
+    },
+    {
+      "type": "input_dummy"
     },
     {
       "type": "input_statement",
@@ -16,14 +19,13 @@ Blockly.defineBlocksWithJsonArray([
   ],
   "previousStatement": null,
   "colour": %{BKY_LOOPS_HUE},
-  "tooltip": "Pause for a specific amount of milliseconds",
+  "tooltip": "Pause for a specific amount of milliseconds.",
   "helpUrl": "https://www.sitepoint.com/delay-sleep-pause-wait/"
 }
 ]);
 Blockly.JavaScript['pause'] = function(block) {
-  var value_pausetime = Blockly.JavaScript.valueToCode(block, 'pausetime', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_milliseconds = Blockly.JavaScript.valueToCode(block, 'time', Blockly.JavaScript.ORDER_ATOMIC);
   var statements_afterpausing = Blockly.JavaScript.statementToCode(block, 'afterpausing');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'setTimeout(() => {  ' + statements_afterpausing + ' },(' + value_pausetime + '));\n';
+  var code = 'setTimeout(() => {  ' + statements_afterwait + ' },(' + value_milliseconds + '));\n';
   return code;
 };

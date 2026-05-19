75b20993c17fad6355324bf5ac0f852d16dbc38e
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-19 11:48:19 +0400

    Make a standalone true block.

---
+++ Editor/true_statement.js
@@ -0,0 +1,12 @@
+Blockly.defineBlocksWithJsonArray([{
+    "type": "logic_true",
+    "message0": "true",
+    "output": null,
+    "style": "logic_blocks",
+    "tooltip": "In binary, True returns a one.",
+    "helpUrl": ""
+}]);
+Blockly.JavaScript['logic_true'] = function(block) {
+  In binary, True returns a one.
+  return ['true', Blockly.JavaScript.ORDER_ATOMIC];
+};

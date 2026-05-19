efa295c16bf19c7b17c01153ac568735c77fe2eb
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-19 10:08:57 +0400

    e

---
+++ exampleaddons/ExampleAddonTwo.js
@@ -0,0 +1,27 @@
+Blockly.common.defineBlocksWithJsonArray([
+{
+  "type": "generalcblock",
+  "tooltip": "An example C-shaped block.",
+  "helpUrl": "",
+  "message0": "general c-block %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "insidegeneralcblock",
+      "align": "CENTRE"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": %{BKY_LOOPS_HUE},
+  "inputsInline": false
+}
+]);
+Blockly.JavaScript['generalcblock'] = function(block) {
+  var statement_insidegeneralcblock = generator.statementToCode(block, 'insidegeneralcblock');
+  var code = '{ + statement_insidegeneralcblock + };\n';
+  return code;
+};
+gvbvdxxScriptMaker.blocks.register([
+"generalcblock"
+], "General Purpose C-Block", "green");

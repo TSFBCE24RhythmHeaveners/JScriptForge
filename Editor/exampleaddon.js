06b47362b13917e627c18492f71eca00ad814f99
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-19 10:10:18 +0400

    e

--- Editor/exampleaddon.js
+++ Editor/exampleaddon.js
@@ -7,15 +7,34 @@ Blockly.defineBlocksWithJsonArray([
   "colour": 230,
   "tooltip": "My Super Cool Block",
   "helpUrl": ""
+},
+{
+  "type": "generalcblock",
+  "tooltip": "An example C-shaped block.",
+  "helpUrl": "",
+  "message0": "general c-block %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "insidegeneralcblock",
+      "align": "CENTRE"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": %{BKY_LOOPS_HUE},
+  "inputsInline": false
 }
 ]);
 Blockly.JavaScript['my_block'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
   var code = 'window.alert("So Cool, I Know Right!");\n';
   return code;
 };
-
-//Add blocks
+Blockly.JavaScript['generalcblock'] = function(block) {
+  var statement_insidegeneralcblock = generator.statementToCode(block, 'insidegeneralcblock');
+  var code = '{ + statement_insidegeneralcblock + };\n';
+  return code;
+};
 gvbvdxxScriptMaker.blocks.register([
-"my_block"
+"my_block", "generalcblock"
 ], "My Cool Blocks", "blue");
